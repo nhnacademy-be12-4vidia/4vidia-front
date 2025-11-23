63d39ed87437c7170ee52e02582884e298ada046
BaeJongOk <0511jongok@gmail.com>
2025-11-23 23:33:57 +0900

    fix: testController 수정6
    -

--- src/main/java/com/nhnacademy/_vidiafront/controller/TestController.java
+++ src/main/java/com/nhnacademy/_vidiafront/controller/TestController.java
@@ -15,7 +15,7 @@ public class TestController {
 
     public TestController() {
         this.webClient = WebClient.builder()
-                .baseUrl("http://4vidia-gateway/4vidia-coupon")
+                .baseUrl("http://4vidia-gateway/4vidia-coupon-service")
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }

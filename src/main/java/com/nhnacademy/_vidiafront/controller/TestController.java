a7d4d613ba91ebdaece45914aee8864eeaea09ba
BaeJongOk <0511jongok@gmail.com>
2025-11-24 00:04:51 +0900

    fix:testController 수정

--- src/main/java/com/nhnacademy/_vidiafront/controller/TestController.java
+++ src/main/java/com/nhnacademy/_vidiafront/controller/TestController.java
@@ -6,10 +6,11 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-@Controller
+@RestController
 public class TestController {
 
     private final WebClient webClient;

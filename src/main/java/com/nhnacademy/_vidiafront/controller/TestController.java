6ba4eca4a1fbb36f28be6565c0eba0eddd63b578
BaeJongOk <0511jongok@gmail.com>
2025-11-24 10:16:48 +0900

    feat: restclient로 바꿔서 테스트
    -

--- src/main/java/com/nhnacademy/_vidiafront/controller/TestController.java
+++ src/main/java/com/nhnacademy/_vidiafront/controller/TestController.java
@@ -5,18 +5,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Controller
 public class TestController {
 
-    private final RestTemplate restTemplate;
+    private final RestClient restClient;
 
     public TestController() {
-        this.restTemplate = new RestTemplate();
+        this.restClient = RestClient.create();
     }
-
     // 최초 페이지 렌더링
     @GetMapping("/")
     public String index() {
@@ -26,12 +26,11 @@ public class TestController {
     // 버튼 클릭 시 서버에서 호출
     @GetMapping("/test")
     public String test(Model model) {
-        // RestTemplate으로 GET 요청
-        TestResponse response = restTemplate.getForObject(
-                "http://4vidia-gateway-1:8080/api/v1/coupon/test",
-                TestResponse.class
-        );
-        log.info(response.toString());
+        TestResponse response = restClient.get() // GET 요청 시작
+                .uri("http://4vidia-gateway-1:8080/api/v1/coupon/test") // URI 설정
+                .retrieve() // 응답 검색
+                .body(TestResponse.class); // 응답 본문을 TestResponse 클래스로 역직렬화
+
         model.addAttribute("testResponse", response);
         return "hi"; // 동일 index.html 렌더링
     }

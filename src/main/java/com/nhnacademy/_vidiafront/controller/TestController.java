bc292b43bfd82a1728596bd2b9cc3d5bea60404c
BaeJongOk <0511jongok@gmail.com>
2025-11-24 09:30:48 +0900

    fix: 버그 수정을 위한 컨트롤러 index.html수정
    -

--- src/main/java/com/nhnacademy/_vidiafront/controller/TestController.java
+++ src/main/java/com/nhnacademy/_vidiafront/controller/TestController.java
@@ -1,24 +1,18 @@
 package com.nhnacademy._vidiafront.controller;
 
 import com.nhnacademy._vidiafront.dto.TestResponse;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class TestController {
 
-    private final WebClient webClient;
+    private final RestTemplate restTemplate;
 
     public TestController() {
-        this.webClient = WebClient.builder()
-                .baseUrl("http://4vidia-gateway-1:8080")
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .build();
+        this.restTemplate = new RestTemplate();
     }
 
     // 최초 페이지 렌더링
@@ -30,15 +24,13 @@ public class TestController {
     // 버튼 클릭 시 서버에서 호출
     @GetMapping("/test")
     public String test(Model model) {
-        TestResponse block = webClient.get()
-                .uri("/api/v1/coupon/test")
-                .retrieve()
-                .bodyToMono(TestResponse.class)
-                .block();
+        // RestTemplate으로 GET 요청
+        TestResponse response = restTemplate.getForObject(
+                "http://4vidia-gateway-1:8080/api/v1/coupon/test",
+                TestResponse.class
+        );
 
-
-        model.addAttribute("testResponse", block);
+        model.addAttribute("testResponse", response);
         return "index"; // 동일 index.html 렌더링
-
     }
 }

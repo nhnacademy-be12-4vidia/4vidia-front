cfe587a51cedd0c86ba6b627b68dde9c88ea246a
ryujikang <104420236+ryujikang@users.noreply.github.com>
2025-12-01 11:25:05 +0900

    ### 제목 # 커밋 타입: 작업내용 (제목과 본문은 한 줄 띄워주세요) feat: [회원] 마이페이지 회원정보, 회원탈퇴 구현
    ### 본문 - 한 줄에 최대 72 글자까지만 입력하기
    # 무엇을, 왜, 어떻게 했는지
    - 회원정보 조회/수정, 비밀번호 수정
    - 회원탈퇴
    
    # 꼬리말
    # (선택) 이슈번호 작성
    
    #   [커밋 타입]  리스트
    #   feat      : 기능 (새로운 기능)
    #   fix       : 버그 (버그 수정)
    #   design    : CSS 등 사용자 UI 디자인 변경
    #   refactor  : 리팩토링
    #   style     : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs      : 문서 (문서 추가, 수정, 삭제)
    #   test      : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore     : 기타 변경사항 (빌드 스크립트 수정 등)
    #   post      : 블로그 포스트 추가 (신규 포스트 작성 및 수정)
    #   rename    : 파일 혹은 폴더명을 수정하거나 옮기는 작업만 하는 경우
    #   remove    : 파일을 삭제하는 작업만 수행한 경우
    # ------------------
    #   [체크리스트]
    #     제목 첫 글자는 대문자로 작성했나요?
    #     제목은 명령문으로 작성했나요?
    #     제목 끝에 마침표(.) 금지
    #     제목과 본문을 한 줄 띄워 분리하기
    #     본문에 여러줄의 메시지를 작성할 땐 "-"로 구분했나요?
    # ------------------

--- src/main/java/com/nhnacademy/_vidiafront/user/client/UserApiClient.java
+++ src/main/java/com/nhnacademy/_vidiafront/user/client/UserApiClient.java
@@ -1,5 +1,8 @@
 package com.nhnacademy._vidiafront.user.client;
 
+import com.nhnacademy._vidiafront.user.dto.request.ChangePasswordRequest;
+import com.nhnacademy._vidiafront.user.dto.request.DeleteUserRequest;
+import com.nhnacademy._vidiafront.user.dto.request.UpdateUserRequest;
 import com.nhnacademy._vidiafront.user.dto.request.UserSignupRequest;
 import com.nhnacademy._vidiafront.user.dto.response.UserProfileResponse;
 import lombok.RequiredArgsConstructor;
@@ -14,30 +17,71 @@ public class UserApiClient {
     private static final String USER_SERVICE = "/api/v1/user-service";
 
     // 테스트용
-    private static final int TEST_ID = 6;
+    private static final int TEST_ID = 8;
     private static final String X_USER_ID = "X-User-Id";
 
     /**
-     * POST 회원가입 호출
+     * POST 회원가입
      * */
-    public String signup(UserSignupRequest userSignupRequest) {
+    public Void signup(UserSignupRequest userSignupRequest) {
         return restClient.post()
                 .uri(USER_SERVICE + "/my/signup")
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(userSignupRequest)
                 .retrieve()
-                .body(String.class);
+                .body(Void.class);
     }
 
     /**
-     * GET 회원정보 조회 호출
+     * GET 회원정보 조회
      * */
     public UserProfileResponse getUserProfile() {
         return restClient.get()
                 .uri(USER_SERVICE + "/my/profile")
+                .accept(MediaType.APPLICATION_JSON)
                 .header(X_USER_ID, String.valueOf(TEST_ID))
                 .retrieve()
                 .body(UserProfileResponse.class);
+    }
+
+    /**
+     * PATCH 회원정보 수정
+     * */
+    public UserProfileResponse updateUserProfile(UpdateUserRequest updateUserRequest) {
+        return restClient.put()
+                .uri(USER_SERVICE + "/my/profile")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header(X_USER_ID, String.valueOf(TEST_ID))
+                .body(updateUserRequest)
+                .retrieve()
+                .body(UserProfileResponse.class);
+    }
+
+    /**
+     * PUT 비밀번호 수정
+     * */
+    public Void changePassword(ChangePasswordRequest changePasswordRequest) {
+        return restClient.put()
+                .uri(USER_SERVICE + "/my/change-password")
+                .contentType(MediaType.APPLICATION_JSON) // todo : 비밀번호가 body로 넘어가도 괜찮을까?
+                .header(X_USER_ID, String.valueOf(TEST_ID))
+                .body(changePasswordRequest)
+                .retrieve()
+                .body(Void.class);
+    }
+
+
+    /**
+     * PUT 회원탈퇴
+     */
+    public Void deleteUser(DeleteUserRequest deleteUserRequest) {
+        return restClient.put()
+                .uri(USER_SERVICE + "/my/delete")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header(X_USER_ID, String.valueOf(TEST_ID))
+                .body(deleteUserRequest)
+                .retrieve()
+                .body(Void.class);
     }
 
 

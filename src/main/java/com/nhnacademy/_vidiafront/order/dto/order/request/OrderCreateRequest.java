526106d6c4206320d0c4a06bc47306d05e6c6e92
jungboX <stonesys123@naver.com>
2025-12-02 16:59:12 +0900

    fix: order create이후 결제까지 연결

---
+++ src/main/java/com/nhnacademy/_vidiafront/order/dto/order/request/OrderCreateRequest.java
@@ -0,0 +1,31 @@
+package com.nhnacademy._vidiafront.order.dto.order.request;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record OrderCreateRequest(
+        String recipientName,
+        String addressRoadname,
+        String addressDetail,
+        String zipCode,
+        String recipientPhone,
+        String deliveryRequest,
+        LocalDate deliveryDate,
+
+        int totalPrice, //도서 가격 합
+        int deliveryCost, //배송비
+        int packagingCost, //포장비
+
+        int couponDiscount, //쿠폰할인금액
+        int pointUsed,  //포인트사용금액
+        int payPrice, //도서가격 + 배송비 + 포장비 - 할인/포인트
+
+        List<ItemRequestDto> orderItems
+) {
+    public record ItemRequestDto(
+            long bookId,
+            int quantity,
+            int salePrice,
+            List<Long> packagingOptionIds
+    ) {}
+}// order.html에서 넘어오는 값

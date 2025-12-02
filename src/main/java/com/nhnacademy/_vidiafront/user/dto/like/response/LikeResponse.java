43ee1de661a0c8adf77eeed176ddd5d2261b27fb
ryujikang <104420236+ryujikang@users.noreply.github.com>
2025-12-02 16:37:16 +0900

    좋아요 리스트 구현 중

--- src/main/java/com/nhnacademy/_vidiafront/user/dto/like/response/LikeResponse.java
+++ src/main/java/com/nhnacademy/_vidiafront/user/dto/like/response/LikeResponse.java
@@ -1,14 +1,12 @@
-//package com.nhnacademy._vidiafront.user.dto.response;
-//
-//import java.util.List;
-//
-//public record LikeResponse(
-//        Long bookId,
-//        String bookTitle,             // 책 제목
-//        List<BookAuthor> bookAuthors, // 저자
-//        Integer priceStandard,        // 판매가
-//        Integer priceSales,           // 할인가
-//        StockStatus stockStatus,      // 재고상태(품절인지)
-//        List<BookImage> bookImageList // 책 이미지
-//){
-//}
+package com.nhnacademy._vidiafront.user.dto.like.response;
+
+public record LikeResponse(
+        Long bookId,
+        String bookTitle, // 책 제목
+        String authorName, // 저자
+        Integer priceStandard, // 판매가
+        Integer priceSales, // 할인가
+        String stockStatus, // 재고상태(품절인지)
+        String bookImage // 책 이미지
+){
+}

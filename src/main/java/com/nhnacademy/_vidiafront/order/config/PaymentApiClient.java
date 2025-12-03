a7ff00bb988079d42a106dbd53fe0d7d02c7435f
jungboX <stonesys123@naver.com>
2025-12-03 14:42:32 +0900

    feat: 주문성공/실패 화면
    - Payment 응답객체 수정
    - 성공/실패 화면 구현

--- src/main/java/com/nhnacademy/_vidiafront/order/config/PaymentApiClient.java
+++ src/main/java/com/nhnacademy/_vidiafront/order/config/PaymentApiClient.java
@@ -3,24 +3,28 @@ package com.nhnacademy._vidiafront.order.config;
 import com.nhnacademy._vidiafront.global.client.BackendApiClient;
 import com.nhnacademy._vidiafront.order.dto.payment.requset.PaymentCancelRequest;
 import com.nhnacademy._vidiafront.order.dto.payment.requset.PaymentConfirmRequest;
+import com.nhnacademy._vidiafront.order.dto.payment.response.PaymentResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClient;
 
 @Component
 @RequiredArgsConstructor
 public class PaymentApiClient {
-    private final RestClient restClient;
     private static final String ORDER_SERVICE = "/api/v1/order-service";
     private final BackendApiClient backendApiClient;
 
     // 결제 확정 및 결제 저장
-    public void confirmPayment(PaymentConfirmRequest confirmRequest, long id) {
-        backendApiClient.post(ORDER_SERVICE + "/orders/" + id + "/success", confirmRequest, void.class);
+    public PaymentResponse confirmPayment(PaymentConfirmRequest confirmRequest, long id) { //주문과정 4번
+        return backendApiClient.post(ORDER_SERVICE + "/orders/" + id + "/success", confirmRequest, PaymentResponse.class);
     }
 
     // 결제 취소인데 배송 전 전체 취소만 해당 - 출고일 이후는 포인트로 돌려줌
-    public void cancelPayment(PaymentCancelRequest cancelRequest, long orderId) {
-        backendApiClient.post(ORDER_SERVICE + "/orders/" + orderId + "/cancel", cancelRequest, void.class);
+    public PaymentResponse cancelPayment(PaymentCancelRequest cancelRequest, long orderId) {
+        return backendApiClient.post(ORDER_SERVICE + "/orders/" + orderId + "/cancel", cancelRequest, PaymentResponse.class);
+    }
+
+    //결제 조회
+    public PaymentResponse getPayment(long orderId) {
+        return backendApiClient.get(ORDER_SERVICE + "/orders/pay/" + orderId, PaymentResponse.class);
     }
 }

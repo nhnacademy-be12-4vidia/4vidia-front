dfd7b23b11f5551e22a7d2e5c733714d9d3bf32a
ryujikang <104420236+ryujikang@users.noreply.github.com>
2025-12-05 19:49:14 +0900

    feat: 마지막 로그인 시간 업데이트 구현 refactor: 로그인 시 휴먼 상태 확인 후 마지막 로그인 시간 업데이트

--- src/main/java/com/nhnacademy/_vidiafront/user/client/UserApiClient.java
+++ src/main/java/com/nhnacademy/_vidiafront/user/client/UserApiClient.java
@@ -3,6 +3,7 @@ package com.nhnacademy._vidiafront.user.client;
 import com.nhnacademy._vidiafront.global.client.BackendApiClient;
 import com.nhnacademy._vidiafront.user.dto.user.request.ChangePasswordRequest;
 import com.nhnacademy._vidiafront.user.dto.user.request.DeleteUserRequest;
+import com.nhnacademy._vidiafront.user.dto.user.request.UpdateLastLoginRequest;
 import com.nhnacademy._vidiafront.user.dto.user.request.UpdateUserRequest;
 import com.nhnacademy._vidiafront.user.dto.user.response.UserProfileResponse;
 import lombok.RequiredArgsConstructor;
@@ -49,4 +50,9 @@ public class UserApiClient {
         return backendApiClient.put(USER_SERVICE + "/my/delete", deleteUserRequest, Void.class);
     }
 
+    // 마지막로그인시간 업데이트하기
+    public Void updateLastLoginAt(String email) {
+        UpdateLastLoginRequest updateLastLoginRequest = new UpdateLastLoginRequest(email);
+        return backendApiClient.put(USER_SERVICE + "/auth/update-time", updateLastLoginRequest, Void.class);
+    }
 }

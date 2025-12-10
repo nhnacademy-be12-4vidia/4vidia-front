06853ecfea2115142a714f65ec338f0a65586500
Link <mexlove2@gmail.com>
2025-12-10 19:29:19 +0900

    feat: http 리버스 프록시 적용 - 리버스 프록시 비스무리한거 구현 - http로 오는 이미지 url을 https 환경에서도 볼 수 있게 구현

--- src/main/java/com/nhnacademy/_vidiafront/global/config/RestClientConfig.java
+++ src/main/java/com/nhnacademy/_vidiafront/global/config/RestClientConfig.java
@@ -4,6 +4,7 @@ import com.nhnacademy._vidiafront.global.interceptor.TokenInterceptor;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
@@ -19,10 +20,18 @@ public class RestClientConfig {
 
     // 2단계: 위에서 만든 빌더를 주입받아 구체적인 설정(baseUrl 등)을 추가하여 RestClient 생성
     @Bean
+    @Primary
     public RestClient restClient(RestClient.Builder builder) {
         return builder
                 .baseUrl("lb://4vidia-gateway") // lb:// 프로토콜 사용 가능
                 .requestInterceptor(new TokenInterceptor())
                 .build();
+    }
+    @Bean
+    public RestClient externalRestClient() {
+        // 여기서 주입받은 builder를 쓰면 안됩니다!
+        return RestClient.builder()
+            .requestFactory(new HttpComponentsClientHttpRequestFactory()) // 필요하다면 팩토리 설정 추가
+            .build();
     }
 }

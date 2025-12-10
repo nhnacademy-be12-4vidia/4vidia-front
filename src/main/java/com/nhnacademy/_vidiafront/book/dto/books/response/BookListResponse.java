26331801d37501d68100879f64aee20bb5c39452
ryujikang <104420236+ryujikang@users.noreply.github.com>
2025-12-10 09:08:06 +0900

    refactor: BookListResponse 에 정가(priceStandard) 추가함 refactor: 좋아요버튼 수정
    - 좋아요버튼 검색할때 뜨는 도서에는 하트색깔 표시된거 저장됨(메인화면 수정 예정)

--- src/main/java/com/nhnacademy/_vidiafront/book/dto/books/response/BookListResponse.java
+++ src/main/java/com/nhnacademy/_vidiafront/book/dto/books/response/BookListResponse.java
@@ -2,6 +2,16 @@ package com.nhnacademy._vidiafront.book.dto.books.response;
 
 import java.util.List;
 
-public record BookListResponse(Long id, String title, String isbn, Integer priceSales, List<String> authorNames, String publisherName, String imageUrl, boolean liked) {
+public record BookListResponse(
+        Long id,
+        String title,
+        String isbn,
+        Integer priceStandard, // 정가 추가
+        Integer priceSales,
+        List<String> authorNames,
+        String publisherName,
+        String imageUrl,
+        boolean liked
+) {
 
 }

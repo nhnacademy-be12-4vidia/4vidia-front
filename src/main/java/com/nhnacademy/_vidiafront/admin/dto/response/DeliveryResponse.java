4bf8acd50a23bd245796ece551704932966fe01d
konayeong <dnjfrhkd402@naver.com>
2025-12-10 19:39:02 +0900

    design : 관리자 배송 관리 기능 디테일 수정
    - 주문 상세 수정, 비회원 정보 조회 오류 수정

--- src/main/java/com/nhnacademy/_vidiafront/admin/dto/response/DeliveryResponse.java
+++ src/main/java/com/nhnacademy/_vidiafront/admin/dto/response/DeliveryResponse.java
@@ -1,15 +1,29 @@
 package com.nhnacademy._vidiafront.admin.dto.response;
 
+import com.nhnacademy._vidiafront.order.dto.DeliveryStatus;
+import com.nhnacademy._vidiafront.order.dto.order.response.OrderResponse;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public record DeliveryResponse(
-        Long orderId,
+        // list 화면에 출력할 정보
+        long orderId,
+        String email,
         String recipientName,
         String addressRoadname,
         String addressDetail,
+        DeliveryStatus deliveryStatus,
+
+        // 상세 추가
+        int payPrice,
         String recipientPhone,
-        String deliveryStatus,
-//        LocalDate deliveryDate,
-        Integer payPrice,
-//        LocalDateTime createdAt
-        String orderStatus
-) {
-}
+        String deliveryRequest,
+        LocalDateTime createAt,
+        int couponDiscount,
+        int pointUsed,
+        LocalDate deliveryDate,
+        LocalDate actualDeliveryDate,
+        List<OrderResponse.OrderBookResponse> orderItems
+){}

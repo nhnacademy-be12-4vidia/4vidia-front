8df31d825812735558dcac0c9bf6e7dfa22b425b
Quasimorphism <bak575755@gmail.com>
2025-12-11 17:20:58 +0900

    feat: 일단 도서 등록 프론트 기본 준비?? - 이제 백엔드 로직 작성하면서 같이 하는 게 좋을 것 같아요

--- src/main/java/com/nhnacademy/_vidiafront/admin/controller/AdminBookController.java
+++ src/main/java/com/nhnacademy/_vidiafront/admin/controller/AdminBookController.java
@@ -1,15 +1,19 @@
 package com.nhnacademy._vidiafront.admin.controller;
 
+import com.nhnacademy._vidiafront.admin.client.AdminBookApiClient;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/admin/books")
 public class AdminBookController {
+    private final AdminBookApiClient bookApiClient;
 
     @GetMapping("/register")
     public String registerForm() {
-        return "admin/book-register";
+        return "admin/admin-book-register";
     }
 }

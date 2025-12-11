4a81dc5f7f8b4311d2a4072b7c1d1a2cfca9c690
ryujikang <104420236+ryujikang@users.noreply.github.com>
2025-12-11 23:29:37 +0900

    css: 메인/관리자 페이지 연결, 도서검색 리스트 페이지 수정, 주문/배송관리페이지 수정 feat: 회원 role 조회 추가

--- src/main/java/com/nhnacademy/_vidiafront/global/controller/MainController.java
+++ src/main/java/com/nhnacademy/_vidiafront/global/controller/MainController.java
@@ -2,6 +2,7 @@ package com.nhnacademy._vidiafront.global.controller;
 
 import com.nhnacademy._vidiafront.book.client.BookApiClient;
 import com.nhnacademy._vidiafront.book.dto.books.response.BookListResponse;
+import com.nhnacademy._vidiafront.user.client.UserApiClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,12 +14,22 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MainController {
     private final BookApiClient bookApiClient;
+    private final UserApiClient userApiClient;
 
     @GetMapping("/")
     public String index(Model model) {
         List<BookListResponse> bestSellerBook = bookApiClient.getBestSellerBook();
         model.addAttribute("bestSellerBook", bestSellerBook);
         return "index";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        if (userApiClient.getUserRole().equals("ADMIN")) {
+            return "admin/admin-layout";
+        } else {
+            return "redirect:/";
+        }
     }
 
     @GetMapping("/mypage")

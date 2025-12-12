90839d07c2dc26944af39a991488db91ba91aa75
urim <kesnss55@gmail.com>
2025-12-12 18:42:55 +0900

    feat: 탈퇴한 회원 로그인 시 에러 메시지 보여줌
    - 탈퇴한 회원이 로그인시 에러 메시지 보여주게 추가

--- src/main/java/com/nhnacademy/_vidiafront/user/controller/AuthController.java
+++ src/main/java/com/nhnacademy/_vidiafront/user/controller/AuthController.java
@@ -19,6 +19,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
@@ -296,6 +297,14 @@ public class AuthController {
             return "redirect:/auth/dormant-auth/email";
         }
     }
+
+    @ExceptionHandler(HttpClientErrorException.Conflict.class)
+    public String handleDELETED(HttpClientErrorException.Conflict ex, Model model) {
+        model.addAttribute("loginError",  "탈퇴한 회원입니다.");
+        return "auth/loginForm";
+    }
+
+    
 
 
 }

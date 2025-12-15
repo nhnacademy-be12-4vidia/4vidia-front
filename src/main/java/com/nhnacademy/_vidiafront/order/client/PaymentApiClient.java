68b9518b1224379a59fb0a34958998e7137445de
jungboX <stonesys123@naver.com>
2025-12-15 21:21:49 +0900

    fix: 결제창 뒤로가기 시, 롤백 구현

--- src/main/java/com/nhnacademy/_vidiafront/order/client/PaymentApiClient.java
+++ src/main/java/com/nhnacademy/_vidiafront/order/client/PaymentApiClient.java
@@ -1,8 +1,8 @@
 package com.nhnacademy._vidiafront.order.client;
 
 import com.nhnacademy._vidiafront.global.client.BackendApiClient;
-import com.nhnacademy._vidiafront.order.dto.payment.requset.PaymentCancelRequest;
 import com.nhnacademy._vidiafront.order.dto.payment.requset.PaymentConfirmRequest;
+import com.nhnacademy._vidiafront.order.dto.payment.requset.PaymentFailRequest;
 import com.nhnacademy._vidiafront.order.dto.payment.response.PaymentResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -21,6 +21,11 @@ public class PaymentApiClient {
     // 결제 성공시 한개 조회
     public PaymentResponse getPayment(long orderId) {
         return backendApiClient.get(ORDER_SERVICE + "/payments/" + orderId, PaymentResponse.class);
+    }
+
+    // 결제창 실패시 롤백 요청
+    public Void rollbackPayment(PaymentFailRequest paymentFailRequest) {
+        return backendApiClient.post(ORDER_SERVICE + "/payments/rollback", paymentFailRequest, Void.class);
     }
 
 //    // 결제 취소인데 배송 전 전체 취소만 해당 - 출고일 이후는 포인트로 돌려줌
---
+++ src/main/java/com/nhnacademy/_vidiafront/order/dto/payment/requset/PaymentFailRequest.java
@@ -0,0 +1,6 @@
+package com.nhnacademy._vidiafront.order.dto.payment.requset;
+
+public record PaymentFailRequest(
+        Long orderId
+) {
+}

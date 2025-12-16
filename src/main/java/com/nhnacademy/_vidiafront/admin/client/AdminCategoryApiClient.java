305d7bf3e80749962114eb55576732fe63fef7ee
tjddn327 <sungwk100@gmail.com>
2025-12-16 16:10:17 +0900

    coupon-service

--- src/main/java/com/nhnacademy/_vidiafront/admin/client/AdminCategoryApiClient.java
+++ src/main/java/com/nhnacademy/_vidiafront/admin/client/AdminCategoryApiClient.java
@@ -12,7 +12,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AdminCategoryApiClient {
 
-    private static final String COUPON_SERVICE = "/api/v1/coupon";
+    private static final String COUPON_SERVICE = "/api/v1/coupon-service";
 
     private final BackendApiClient backendApiClient;
 
--- src/main/java/com/nhnacademy/_vidiafront/admin/client/AdminCouponApiClient.java
+++ src/main/java/com/nhnacademy/_vidiafront/admin/client/AdminCouponApiClient.java
@@ -16,7 +16,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AdminCouponApiClient {
 
-    private static final String COUPON_SERVICE = "/api/v1/coupon";
+    private static final String COUPON_SERVICE = "/api/v1/coupon-service";
 
     private final BackendApiClient backendApiClient;
 

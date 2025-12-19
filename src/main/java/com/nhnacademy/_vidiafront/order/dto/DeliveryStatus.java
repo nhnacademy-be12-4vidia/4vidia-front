4c52e66c5d33527895e40685a042bfbfe1f93008
jungboX <stonesys123@naver.com>
2025-12-20 00:17:39 +0900

    chore: 주문 상세 css 통합
    - css 편의를 위해 주문 및 배송 상태 필드값 추가

--- src/main/java/com/nhnacademy/_vidiafront/order/dto/DeliveryStatus.java
+++ src/main/java/com/nhnacademy/_vidiafront/order/dto/DeliveryStatus.java
@@ -6,12 +6,15 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum DeliveryStatus {
-    WAITING(0),
-    SHIPPING(1),
-    DELIVERED(2),
-    CANCELED(3);
+    WAITING(0, "배송 준비중", "text-warning bg-warning-light", "fa-box-open"),
+    SHIPPING(1, "배송 중", "text-info bg-info-light", "fa-truck"),
+    DELIVERED(2, "배송 완료", "text-success bg-success-light", "fa-check-circle"),
+    CANCELED(3, "주문 취소", "text-secondary bg-secondary-light", "fa-times-circle");
 
     private final int code;
+    private final String description;
+    private final String colorClass;
+    private final String iconClass;
 
     public static DeliveryStatus fromCode(int code) {
         for (DeliveryStatus status : DeliveryStatus.values()) {

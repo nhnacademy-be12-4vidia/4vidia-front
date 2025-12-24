d09ddb868dbfed934a151ebd90c4ba7c8f7bccf8
ryujikang <104420236+ryujikang@users.noreply.github.com>
2025-12-25 07:12:19 +0900

    feat: 좋아요 리스트 페이지네이션 적용

--- src/main/java/com/nhnacademy/_vidiafront/user/client/LikeApiClient.java
+++ src/main/java/com/nhnacademy/_vidiafront/user/client/LikeApiClient.java
@@ -1,10 +1,13 @@
 package com.nhnacademy._vidiafront.user.client;
 
 import com.nhnacademy._vidiafront.global.client.BackendApiClient;
+import com.nhnacademy._vidiafront.global.dto.PageResponse;
 import com.nhnacademy._vidiafront.user.dto.like.response.LikeResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -14,13 +17,23 @@ public class LikeApiClient {
     private final BackendApiClient backendApiClient;
     private static final String USER_SERVICE = "/api/v1/user-service";
     private static final String BASE_URL = "/users/me/likes";
-    // 기존 base url = "/my/likes" -> 수정 "/users/me/likes"
+
+    /**
+     * GET 좋아요 리스트 조회 (my page)
+     * */
+    public PageResponse<LikeResponse> getLikeListPage(Pageable pageable) {
+        String uri = UriComponentsBuilder.fromUriString(USER_SERVICE + BASE_URL)
+                .queryParam("page", pageable.getPageNumber())
+                .queryParam("size", pageable.getPageSize())
+                .toUriString();
+        return backendApiClient.get(uri, new ParameterizedTypeReference<>() {});
+    }
 
     /**
      * GET 좋아요 리스트 조회
      * */
     public List<LikeResponse> getLikeList() {
-        return backendApiClient.get(USER_SERVICE + BASE_URL, new ParameterizedTypeReference<>() {});
+        return backendApiClient.get(USER_SERVICE + BASE_URL + "/all", new ParameterizedTypeReference<>() {});
     }
 
     /**
--- src/main/java/com/nhnacademy/_vidiafront/user/controller/LikeController.java
+++ src/main/java/com/nhnacademy/_vidiafront/user/controller/LikeController.java
@@ -1,14 +1,15 @@
 package com.nhnacademy._vidiafront.user.controller;
 
+import com.nhnacademy._vidiafront.global.dto.PageResponse;
 import com.nhnacademy._vidiafront.user.client.LikeApiClient;
 import com.nhnacademy._vidiafront.user.dto.like.response.LikeResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
@@ -17,12 +18,20 @@ public class LikeController {
     private final LikeApiClient likeApiClient;
 
     /**
-     * 좋아요 리스트 조회
+     * 좋아요 리스트 조회 (page)
      */
     @GetMapping
-    public String like(Model model) {
-        List<LikeResponse> likes = likeApiClient.getLikeList();
+    public String getLikesPage(@PageableDefault(size = 10) Pageable pageable, Model model) {
+        PageResponse<LikeResponse> likes = likeApiClient.getLikeListPage(pageable);
         model.addAttribute("likes", likes);
+
+        int startPage = Math.max(1, likes.page() - 4);
+        int endPage = Math.min(likes.totalPages(), likes.page() + 5);
+        if (endPage == 0) endPage = 1;
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
         return "mypage/like/likeList";
     }
 

2cdd8a6745bb6205cde48925c34fe96f55f616a2
konayeong <dnjfrhkd402@naver.com>
2025-12-26 11:08:12 +0900

    fix : 관리자 반품 승인, 거절 오류 수정
    - 반품 승인,거절 오류 수정

--- src/main/java/com/nhnacademy/_vidiafront/refund/dto/request/RefundItemUpdateRequest.java
+++ src/main/java/com/nhnacademy/_vidiafront/refund/dto/request/RefundItemUpdateRequest.java
@@ -1,9 +1,9 @@
 package com.nhnacademy._vidiafront.refund.dto.request;
 
-import com.nhnacademy._vidiafront.refund.dto.RefundStatus;
+import com.nhnacademy._vidiafront.refund.dto.response.RefundItemStatus;
 
 public record RefundItemUpdateRequest (
-        RefundStatus refundStatus,
+        RefundItemStatus refundItemStatus,
         String rejectDetail // 거절에만
 ) {
 }

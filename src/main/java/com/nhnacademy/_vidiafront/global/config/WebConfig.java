6167a229f3685feef16e3dbe35237b45ec5a3686
Quasimorphism <bak575755@gmail.com>
2025-12-27 19:37:41 +0900

    fix: 필요 없는 수정 제거

--- src/main/java/com/nhnacademy/_vidiafront/global/config/WebConfig.java
+++ src/main/java/com/nhnacademy/_vidiafront/global/config/WebConfig.java
@@ -2,13 +2,7 @@ package com.nhnacademy._vidiafront.global.config;
 
 import com.nhnacademy._vidiafront.global.interceptor.TempUserInterceptor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.web.multipart.MultipartResolver;
-import org.springframework.web.multipart.support.MultipartFilter;
-import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 

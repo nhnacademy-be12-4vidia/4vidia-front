225e94283c39199b64f6faa2dc34acf699db50b3
BaeJongOk <0511jongok@gmail.com>
2026-01-13 23:26:13 +0900

    fix: 오류 수정중5

--- src/main/java/com/nhnacademy/_vidiafront/admin/controller/AdminBookController.java
+++ src/main/java/com/nhnacademy/_vidiafront/admin/controller/AdminBookController.java
@@ -1,7 +1,6 @@
 package com.nhnacademy._vidiafront.admin.controller;
 
 import com.nhnacademy._vidiafront.admin.client.AdminBookApiClient;
-import com.nhnacademy._vidiafront.admin.client.AdminCheckApiClient;
 import com.nhnacademy._vidiafront.admin.dto.request.AdminBookCreateRequest;
 import com.nhnacademy._vidiafront.admin.dto.request.AdminBookUpdateRequest;
 import com.nhnacademy._vidiafront.admin.dto.response.AdminIsbnSearchResponse;
@@ -27,14 +26,12 @@ public class AdminBookController {
     private final AdminBookApiClient adminBookApiClient;
     private final BookApiClient bookApiClient;
     private final CategoryApiClient categoryApiClient;
-    private final AdminCheckApiClient adminCheckApiClient;
 
     // 도서 생성 폼
     @GetMapping
     public String getBookCreateForm(
             Model model
     ) {
-        adminCheckApiClient.checkAdmin();
         model.addAttribute("mode", "create");
         model.addAttribute("categories", categoryApiClient.getFlatCategoryList());
         return "admin/admin-book-form";
